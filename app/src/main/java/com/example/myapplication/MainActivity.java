68c7e5f25f13458dbb0afc375f44c19c4c407a3b
Kurtyka03 <kurtyka03@gmail.com>
2022-03-10 13:22:14 +0100

    dodanie pierwszych funkcji krupiera

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    int punktyUser = 0;
+    int punktyKrupier = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,20 +23,52 @@ public class MainActivity extends AppCompatActivity {
 
     private int Losowanie(){
         Random r = new Random();
-        int Liczba = r.nextInt(52+1);
+        int Liczba = r.nextInt(4+1);
         return Liczba;
     }
     private void Krupier(){
-
+        ImageView Krupier = findViewById(R.id.imageView);
+        switch (Losowanie()){
+            case 1:
+                Krupier.setImageResource(R.drawable.ac);
+                punktyKrupier += 11;
+                Koniec();
+                break;
+            case 2:
+                Krupier.setImageResource(R.drawable.ad);
+                punktyKrupier += 11;
+                Koniec();
+                break;
+            case 3:
+                Krupier.setImageResource(R.drawable.ah);
+                punktyKrupier += 11;
+                Koniec();
+                break;
+            case 4:
+                Krupier.setImageResource(R.drawable.as);
+                punktyKrupier += 11;
+                Koniec();
+                break;
+        }
     }
 
     public void Start(View view) {
+        Button losuj = findViewById(R.id.button);
+        Button start = findViewById(R.id.button2);
+        Button reset = findViewById(R.id.button3);
 
+        losuj.setEnabled(true);
+        start.setEnabled(false);
+        reset.setEnabled(true);
     }
 
     public void Losuj(View view) {
     }
 
     public void Reset(View view) {
+    }
+
+    public void Koniec(){
+
     }
 }
